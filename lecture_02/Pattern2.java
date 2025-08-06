f9c8028826433d1057d01f4f67e15b49ce3202e3
Mahima <mahimarn84@gmail.com>
2025-08-07 00:14:37 +0530

    Pattern2

---
+++ lecture_02/Pattern2.java
@@ -0,0 +1,17 @@
+package lecture_02;
+import java.util.*;
+
+public class Pattern2 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+
+        for(int i =1; i<=n  ; i++){
+            for (int j= 1; j<=n+1-i; j++){
+                System.out.print("*"+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
