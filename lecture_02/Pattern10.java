b4ca4cbfaf88b22495394a024dbdad10a604a551
Mahima <mahimarn84@gmail.com>
2025-08-13 19:39:46 +0530

    ok

---
+++ lecture_02/Pattern10.java
@@ -0,0 +1,14 @@
+package lecture_02;
+import java.util.*;
+
+public class Pattern10 {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for(int i = 1; i<= n; i++){
+            
+        }
+    }
+    
+}
