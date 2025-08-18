8109a256cd9190af30b0227d85a7c1ba50fa9d58
Mahima <mahimarn84@gmail.com>
2025-08-18 22:13:10 +0530

    ques13

---
+++ lecture_01/ques13.java
@@ -0,0 +1,21 @@
+package lecture_01;
+import java.util.*;
+
+public class ques13 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+            int d = scn.nextInt();
+            int count = 0;
+
+            while(n !=0){
+                int rem = n%10;
+                if(rem == d){
+                    count++;    
+                }
+                n /= 10;
+            }
+            System.out.println(count);
+    }
+    
+}
