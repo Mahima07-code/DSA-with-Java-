65aacae89dc99f7cd524425933549a442b7721fa
Mahima <mahimarn84@gmail.com>
2025-08-18 23:29:00 +0530

    ques13

--- lecture_01/ques13.java
+++ lecture_01/ques13.java
@@ -1,3 +1,9 @@
+// QUESTION13
+// Calculate the frequency of the given digit in the given no 
+// Take "n" as input number and "d" be the digit to count teh frequency of.
+
+
+
 package lecture_01;
 import java.util.*;
 
@@ -6,16 +12,25 @@ public class ques13 {
         Scanner scn = new Scanner(System.in);
             int n = scn.nextInt();
             int d = scn.nextInt();
-            int count = 0;
+            int freq = DigitFrequency(n,d);
+            System.out.println(freq);
 
-            while(n !=0){
-                int rem = n%10;
-                if(rem == d){
-                    count++;    
-                }
-                n /= 10;
-            }
-            System.out.println(count);
     }
-    
+
+    public static int DigitFrequency(int x, int y){
+        int count = 0;
+
+        while(x >0){
+            int rem = x%10;
+
+            if(rem == y){
+                count++;    
+            }
+            
+            x /= 10;
+        }
+        return count;
+    } 
 }
+    
+
